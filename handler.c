47b3d73b9a1ea495a957f7d19334b7f6b4633edd
Semere M. Mebrahtom <semere.meharena1@gmail.com>
2026-01-10 19:26:45 +0100

    untested commit for the 'd/i' specifiers

--- handler.c
+++ handler.c
@@ -70,8 +70,27 @@ char *handle_p(char *str, t_template *tpl)   {
 }
 
 char *handle_d(char *str, t_template *tpl) {
-	(void ) tpl;
-	return ft_strdup(str);	
+	if (tpl->sign && str[0] != '-')
+		str = append(ft_strdup("-"), str, ft_strlen(str), 1);
+	else if (tpl->space && str[0] != '-')
+		str = append(ft_strdup(" "), str, ft_strlen(str), 1);
+
+	char *str_gen = ft_strdup("");
+
+	if (tpl->precision > (int) ft_strlen(str))
+		str = append(ft_str_gen('0', tpl->precision - ft_strlen(str)), str, ft_strlen(str), 1);
+	
+	if(tpl->width > (int) ft_strlen(str)){
+		free(str_gen);
+		char c = ' ';
+		if (tpl->zero && !tpl->left && !tpl->precision)
+			c = '0';
+		str_gen = ft_str_gen(c, tpl->width - (int) ft_strlen(str));
+	}
+
+	if (tpl->left)
+		return append(str, str_gen, ft_strlen(str_gen), 1);
+	return append(str_gen, str, ft_strlen(str), 1);
 }
 
 char *handle_u(char *str, t_template *tpl) {
